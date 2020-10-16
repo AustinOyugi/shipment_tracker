540e9525c6624c7f746eb3958400c69607131f30
Austin Oyugi <austinoyugi@gmail.com>
2020-10-16 13:47:08 +0300

    Update User.java

--- src/main/java/com/myshipment/tracker/models/User.java
+++ src/main/java/com/myshipment/tracker/models/User.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * @author Austin Oyugi
@@ -21,11 +18,12 @@ import javax.persistence.Id;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     private String userName;
 
