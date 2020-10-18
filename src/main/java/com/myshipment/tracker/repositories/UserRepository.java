7057a29010244ff7602ef530563144394f5c0136
Austin Oyugi <austinoyugi@gmail.com>
2020-10-18 12:48:29 +0300

    Update UserRepository.java

--- src/main/java/com/myshipment/tracker/repositories/UserRepository.java
+++ src/main/java/com/myshipment/tracker/repositories/UserRepository.java
@@ -15,4 +15,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUserNameOrEmail(String userNameOrEmail,String email);
+    Optional<User> findByUserName(String userName);
 }
